eb1ee1d83b9dcabd17e6dafce3dda2e403d3a229
florenta <florent.alemany@gmail.com>
2019-08-19 07:15:36 +0200

    force redirection if not https

--- assets/js/js/routing.js
+++ assets/js/js/routing.js
@@ -2,3 +2,7 @@ const routes = require('../../../web/js/fos_js_routes.json');
 import Routing from '../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
 Routing.setRoutingData(routes);
 export {Routing};
+
+if (window.location.protocol === 'http:') {
+    window.location.href = window.location.href.replace(/http/, 'https');
+}
